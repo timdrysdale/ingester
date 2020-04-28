53e4bf6d5993fb323c76395c3dc93fb5eb902172
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-04-28 19:55:18 +0100

    added overlay.go, compiling, not tested

---
+++ addbars.go
@@ -0,0 +1,47 @@
+package ingester
+
+/*
+func AddMarkBar() {
+}
+
+func AddModActiveBar() {
+}
+
+func AddModInActiveBar() {
+}
+
+func AddCheckBar() {
+}
+
+//TODO ensure/add these paths to gradexpath
+func FilesFrom(exam, stage string) {
+	switch stage {
+	case "mark":
+		return gradexpath.AnonymousPapers(exam)
+	case "moderate-active":
+		return gradexpath.ModerateActivePapers(exam)
+	case "moderate-inactive":
+		return gradexpath.ModerateInactivePapers(exam)
+	case "check":
+		return gradexpath.CheckPapers(exam)
+	default:
+		return ""
+	}
+}
+
+//TODO ensure/add these paths to gradexpath
+func FilesTo(exam, stage string) {
+	switch stage {
+	case "mark":
+		return gradexpath.ToMarkPapers(exam) // consider issue of number of markers separately ... ?
+	case "moderate-active":
+		return gradexpath.ToModerateActivePapers(exam)
+	case "moderate-inactive":
+		return gradexpath.ToModerateInactivePapers(exam)
+	case "check":
+		return gradexpath.ToCheckPapers(exam)
+	default:
+		return ""
+	}
+}
+*/
